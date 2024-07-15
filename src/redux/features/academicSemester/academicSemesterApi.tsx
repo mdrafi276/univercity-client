329a658d7f65bee812027617d64821cee846f3b5
mdrafi276 <mdrafi37473@gmail.com>
2024-07-15 23:27:55 +0600

    add

--- src/redux/features/academicSemester/academicSemesterApi.tsx
+++ src/redux/features/academicSemester/academicSemesterApi.tsx
@@ -1,7 +1,8 @@
 import { baseApi } from '../../api/baseApi';
 
 const academicSemesterApi = baseApi.injectEndpoints({
- 
+
 });
+
 
 export const { useGetAllSemestersQuery } = academicSemesterApi;
--- src/types/global.ts
+++ src/types/global.ts
@@ -22,4 +22,3 @@ message: string
 }
 
 
-
